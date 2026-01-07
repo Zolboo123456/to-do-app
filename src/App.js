a1cc478dba3232aee2e06bb75145fea533ab7eea
Zolboo123456 <knownasmisora@gmail.com>
2026-01-06 19:18:42 -0800

    now able to show the tasks on the screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskValue, setTaskValue] = useState("");
+
+  const clearTasks = () => setTasks([]);
+  const addTask = () => {
+    if (taskValue === "") {
+      alert("Please enter a task!");
+    }
+
+    setTasks((prev) => [...prev, taskValue]);
+    setTaskValue("");
+  };
+
+  console.log(tasks);
+
   return (
     <div className="appWrapper">
       <div className="App">
         <h2>To-Do list</h2>
 
-        <div className="main">
-          <input placeholder="Add a new task ..." />
-          <button>Add</button>
+        <div className="task-manager">
+          <input
+            type="text"
+            maxLength={100}
+            placeholder="Add a new task..."
+            value={taskValue}
+            onChange={(e) => setTaskValue(e.target.value)}
+          />
+          <button onClick={addTask}>Add</button>
+          <button onClick={clearTasks}>Clear</button>
         </div>
 
         <div className="filters">
-          <button>All</button>
+          <button className="button1">All</button>
           <button>Active</button>
           <button>Completed</button>
         </div>
 
+        <ul id="tasks">
+          {tasks.map((task) => {
+            return <li>{task}</li>;
+          })}
+        </ul>
+
+        <div>
+          <p className="noTask">No tasks yet. Add one above!</p>
+        </div>
+
         <footer>
-          <p className="p1">Powered by</p>
+          <p className="p1">Powered by&nbsp;&nbsp;</p>
           <p className="p2">Pinecone Academy</p>
         </footer>
       </div>
